2fd7d7b024ea8b46047024ed4256236ce2c1b103
khalil alsulaimani <alsulaimani.2000@outlook.com>
2021-09-06 18:41:13 +0300

    added missing requirement of making a file if the dest file doesn't exist

--- Lab1/ExerciseA.java
+++ Lab1/ExerciseA.java
@@ -1,16 +1,29 @@
 package Lab1;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class ExerciseA {
 
+    public static void copyFile(File src , File dest) throws IOException {
+        FileChannel srcChannel = null;
+        FileChannel destChannel = null;
 
-
+        try {
+            srcChannel = new FileInputStream( src ).getChannel();
+            destChannel = new FileOutputStream( dest ).getChannel();
+            destChannel.transferFrom( srcChannel , 0 , srcChannel.size() );
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            srcChannel.close();
+            destChannel.close();
+        }
+    }
 
 
     public static void main(String[] args) throws IOException {
@@ -20,19 +33,18 @@ public class ExerciseA {
         File src = new File( scan.nextLine() );
 
         System.out.println( "enter path of destination file to copy to : " );
-        File dest = new File( scan.nextLine() );
+        String destPath = scan.nextLine();
 
+        Path path = Paths.get( destPath );
 
-        FileChannel srcChannel = null;
-        FileChannel destChannel = null;
+        if (Files.exists( path )) {
+            File dest = new File( destPath );
+            ExerciseA.copyFile( src , dest );
 
-        try {
-            srcChannel = new FileInputStream( src ).getChannel();
-            destChannel = new FileOutputStream( dest ).getChannel();
-            destChannel.transferFrom( srcChannel , 0 , srcChannel.size() );
-        } finally {
-            srcChannel.close();
-            destChannel.close();
+        } else {
+            File backupFile = new File( "D:\\Desktop\\uni\\Year 3\\Advanced Programming Theory\\backup.txt" );
+            ExerciseA.copyFile( src , backupFile );
+
         }
 
 
