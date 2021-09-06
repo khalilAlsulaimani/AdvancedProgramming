f1c2a7f5398bd58f1e6df4abda142e83dad3845c
khalil alsulaimani <alsulaimani.2000@outlook.com>
2021-09-06 16:52:10 +0300

    lecture one example one done

--- Theroy/FileAndDirectoryInfo.java
+++ Theroy/FileAndDirectoryInfo.java
@@ -1,19 +1,51 @@
 package Theroy;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class FileAndDirectoryInfo {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("enter file or directory name :");
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner( System.in );
+        System.out.println( "enter file or directory name :" );
 
-        Path path = Paths.get(scan.nextLine());
+        Path path = Paths.get( scan.nextLine() );
 
-        if(Files.exists(path)){
-            System.out.printf("%n%s exits%n" , path.getFileName());
+        if (Files.exists( path )) {
+            System.out.printf( "%n%s exits%n" , path.getFileName() );
+            System.out.printf( "%s a directory%n" , Files.isDirectory( path ) ? "is" : "is not" );
+            System.out.printf( "%s an absoulute path%n" , path.isAbsolute() ? "is" : "is not" );
+
+
+
+
+            System.out.printf( "size %s%n" , Files.size( path ) );
+
+
+            System.out.printf( "path: %s%n" , path );
+            System.out.printf( "absoulte path: %s%n" , path.toAbsolutePath() );
+
+            if (Files.isDirectory( path )) {
+                System.out.printf( "%nDirectory contents:%n" );
+
+                DirectoryStream<Path> directoryStream = null;
+                try {
+                    directoryStream = Files.newDirectoryStream( path );
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                assert directoryStream != null;
+                for (Path p : directoryStream) {
+                    System.out.println( p );
+                }
+            } else {
+                System.out.printf( "%s does not exist%n" , path );
+            }
+
         }
     }
 }
